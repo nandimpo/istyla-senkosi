afb10675d07ecd4d8c222a227ae6039efa50a5b0
nandimpofu <nandi.mpofu12@gmail.com>
2026-09-21 03:55:58 +0200

    chanfr

--- istyla-senkosi/src/components/StitchedNarrative.jsx
+++ istyla-senkosi/src/components/StitchedNarrative.jsx
@@ -22,6 +22,7 @@ export default function StitchedNarrative({ text, placement = "lower-left", chap
   const textComplete = () => {
     textDoneRef.current = true;
     if (reducedMotion || window.matchMedia("(prefers-reduced-motion: reduce)").matches) {
+      needleRef.current?.setAttribute("transform", "translate(475 14) rotate(28)");
       setPhase("hanging");
     } else if (threadDoneRef.current) {
       setPhase("hanging");
@@ -73,7 +74,7 @@ export default function StitchedNarrative({ text, placement = "lower-left", chap
         <path className="stitched-narrative__draw" ref={lineRef} d="M1 14 H475" />
         <path className="stitched-narrative__stitches" d="M1 14 H475" />
         <path className="stitched-narrative__tail" pathLength="100" d="M475 14 C482 32 483 48 473 63 C460 80 471 99 489 93 C505 88 499 69 486 74 C475 79 484 103 500 111" />
-        <g className="stitched-narrative__needle" ref={needleRef} transform="translate(475 14) rotate(28)">
+        <g className="stitched-narrative__needle" ref={needleRef} transform="translate(1 14) rotate(28)">
           <path className="stitched-narrative__needle-body" d="M-33 -2.4 Q-35 0 -33 2.4 L-7 1.5 Q-3 .9 0 0 Q-3 -.9 -7 -1.5 Z" />
           <path className="stitched-narrative__needle-shine" d="M-29 -1 L-7 -.65" />
           <ellipse className="stitched-narrative__needle-eye" cx="-29" cy="0" rx="2.9" ry="1.15" />
